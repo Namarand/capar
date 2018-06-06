84d60fdcc578d88b6a989a978a302585febfe8c4
Clément Galland <gallan_c@epita.fr>
2018-06-06 18:11:05 +0200

    trie: temporaly remove the counter as their a not usefull yet

--- src/trie.cpp
+++ src/trie.cpp
@@ -10,10 +10,10 @@ void Trie::init(const std::vector<std::string>& word_list)
 std::future<std::pair<std::string, int>> Trie::search(const std::string& w) const
 {
     return std::async([this, w](){
-        while (remove_counter_.load() > 0) {}
-        read_write_counter_.fetch_add(1);
+       // while (remove_counter_.load() > 0) {}
+       // read_write_counter_.fetch_add(1);
         auto res = root_.load()->search(w);
-        read_write_counter_.fetch_sub(1);
+       // read_write_counter_.fetch_sub(1);
         return res;
     });
 }
@@ -21,19 +21,19 @@ std::future<std::pair<std::string, int>> Trie::search(const std::string& w) cons
 std::future<void> Trie::insert(const std::string& w)
 {
     return std::async([this, w](){
-        while (remove_counter_.load() > 0) {}
-        read_write_counter_.fetch_add(1);
+       // while (remove_counter_.load() > 0) {}
+       // read_write_counter_.fetch_add(1);
         root_.load()->insert(w);
-        read_write_counter_.fetch_sub(1);
+       // read_write_counter_.fetch_sub(1);
     });
 }
 
 std::future<void> Trie::erase(const std::string& w)
 {
     return std::async([this, w](){
-        remove_counter_.fetch_add(1);
-        while (read_write_counter_.load() > 0) {}
+       // remove_counter_.fetch_add(1);
+       // while (read_write_counter_.load() > 0) {}
         root_.load()->erase(w);
-        remove_counter_.fetch_sub(1);
+       // remove_counter_.fetch_sub(1);
     });
 }
